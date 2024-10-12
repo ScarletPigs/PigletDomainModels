98fc7b3494e2d353c140489fd12d3680680df973
Rasmus Tanggaard <rasmus@tthost.dk>
2024-10-12 14:02:39 +0200

    Various base models added

---
+++ Models/Event.cs
@@ -0,0 +1,24 @@
+﻿namespace Piglet_Domain_Models.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string CreatorDiscordUsername { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModified { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        
+
+
+
+
+        public EventType EventType { get; set; }
+        public ModList ModList { get; set; }
+
+
+    }
+}
---
+++ Models/EventType.cs
@@ -0,0 +1,10 @@
+﻿namespace Piglet_Domain_Models.Models
+{
+    public class EventType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string RequiredRole { get; set; }
+    }
+}
